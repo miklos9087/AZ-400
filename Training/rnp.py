a70bdb323d8ea9291229c28ac74a872c1e532423
miklos9087 <49094202+miklos9087@users.noreply.github.com>
2024-11-28 11:15:44 +0000

    new change1

---
+++ Training/rnp.py
@@ -0,0 +1,83 @@
+roda = "Sorte"
+lenght = len(roda)
+print(lenght)
+
+mosca = "voa"
+upper = mosca.upper()
+print(upper)
+
+
+mosca = "VOA"
+lower = mosca.lower()
+print(lower)
+
+
+predio = "Amadora"
+replace = predio.replace("Amadora","Queluz")
+print(replace)
+
+
+string_variable = "Hello, World!"
+lowercase_string = string_variable.lower()
+print(lowercase_string)
+
+
+split_words = "Casa na Montanha do Tio Antonio"
+split = split_words.split( )
+print(split)
+
+#the bellow list of numbers are mutable that means they can be changed
+numbers_list = [1,2,3,4,5]
+print(numbers_list)
+
+#the bellow list of numbers are inmutable (tuple) that means they can't be changed unless I go there and change them
+numbe_list = (1,2,3,4,5)
+print(numbe_list)
+
+#Sets data type, we can add an item but not modify: commands are .add .remove .update
+sets_list = {1, 2, 3, 4, 5}
+print(sets_list)
+sets_list.add(6)
+print(sets_list)
+
+# My Vmware data
+
+Vmware_data = {
+    "hostname":"ExSiV8",
+    "Servers":{"Ip Address-1":"172.16.1.5","Ip Address-2":"172.16.1.4"},
+    "Username":"root",
+    "Password":"Rambo123!",
+
+}
+
+print(Vmware_data["Servers"]["Ip Address-2"])
+
+
+#Boolean types
+
+Server_up = True
+
+if Server_up:
+    print("Server is running")
+
+else:
+    print("Server is down")
+
+
+
+
+
+
+a = 20
+b = 10
+
+is_equal = a == b
+is_greater = a < b
+
+print(is_equal)
+print(is_greater)
+
+
+
+
+
---
+++ Training/wtf.py
@@ -0,0 +1,14 @@
+# My Vmware data
+
+Vmware_data = {
+    "hostname":"ExSiV8",
+    "Ip Address":{"server1":"172.16.1.5","server2":"172.16.1.4"},
+    "Username":"root",
+    "Password":"Rambo123!",
+
+}
+
+print(Vmware_data["Ip Address"]["server1"])
+
+
+
